a473aa8e736a5dcbde089df35811c5f0baa3faaa
Mikro <cyber.trading@email.de>
2019-01-17 16:20:06 +0100

    Small refactoring

--- FileNameHelper/FileNameHelper.cs
+++ FileNameHelper/FileNameHelper.cs
@@ -101,8 +101,8 @@ namespace FileNameHelper
             }
             set
             {
-                string filename = _fileSystem.Path.GetFileNameWithoutExtension(value);
-                SetFilename(filename);
+                //string filename = _fileSystem.Path.GetFileNameWithoutExtension(value);
+                SetFilename(value);
 
                 string extension = _fileSystem.Path.GetExtension(value);
                 bool extensionFound = (extension != "");
